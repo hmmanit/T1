e6dda73c2a92c5935ec56c47b6f9674ed27c438c
Man Ho <manhm@rch-asia.com>
2021-05-13 10:55:34 +0700

    First UI - Home

--- presentation/src/main/kotlin/com/homanad/android/t1/ui/feature/home/HomeFragment.kt
+++ presentation/src/main/kotlin/com/homanad/android/t1/ui/feature/home/HomeFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import com.homanad.android.common.components.ui.BaseFragment
 import com.homanad.android.t1.R
+import com.homanad.android.t1.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +20,21 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
+
+    override fun observeData() {
+
+    }
+
+    override fun setupViewModel() {
+
+    }
+
+    override fun updateUI() {
+
     }
 
     companion object {
