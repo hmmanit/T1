c03102f166500bec69086cac3a6aba08575c3380
Man Ho <manhm@rch-asia.com>
2021-05-14 14:04:47 +0700

    Home -> COETask transition

--- presentation/src/main/kotlin/com/homanad/android/t1/ui/feature/home/HomeFragment.kt
+++ presentation/src/main/kotlin/com/homanad/android/t1/ui/feature/home/HomeFragment.kt
@@ -11,6 +11,9 @@ import com.google.android.material.tabs.TabLayoutMediator
 import com.homanad.android.common.components.ui.BaseFragment
 import com.homanad.android.common.extensions.view.gone
 import com.homanad.android.common.extensions.view.visible
+import com.homanad.android.common.utilities.drawReenterTransition
+import com.homanad.android.common.utilities.getExitMaterialElevationScale
+import com.homanad.android.common.utilities.getReturnMaterialSharedAxis
 import com.homanad.android.t1.R
 import com.homanad.android.t1.databinding.FragmentHomeBinding
 import com.homanad.android.t1.ui.feature.home.page.adapter.HomeViewPagerAdapter
@@ -30,6 +33,11 @@ class HomeFragment : BaseFragment() {
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        drawReenterTransition(view)
     }
 
     override fun observeData() {
@@ -61,6 +69,13 @@ class HomeFragment : BaseFragment() {
                     Page.TASKS.id -> {
                         binding.fabCreate.visible()
                         binding.fabCreate.setOnClickListener {
+                            exitTransition = getExitMaterialElevationScale(
+                                resources.getInteger(R.integer.motion_duration_large).toLong()
+                            )
+                            reenterTransition = getReturnMaterialSharedAxis(
+                                resources.getInteger(R.integer.motion_duration_large).toLong()
+                            )
+
                             val directions =
                                 HomeFragmentDirections.actionHomeFragmentToCreateOrEditTaskFragment()
                             findNavController().navigate(directions)
--- presentation/src/main/kotlin/com/homanad/android/t1/ui/feature/home/task/CreateOrEditTaskFragment.kt
+++ presentation/src/main/kotlin/com/homanad/android/t1/ui/feature/home/task/CreateOrEditTaskFragment.kt
@@ -1,11 +1,16 @@
 package com.homanad.android.t1.ui.feature.home.task
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.transition.Slide
+import com.google.android.material.transition.MaterialContainerTransform
 import com.homanad.android.common.components.ui.BaseFragment
+import com.homanad.android.common.extensions.context.themeColor
 import com.homanad.android.t1.R
 import com.homanad.android.t1.databinding.FragmentCreateOrEditTaskBinding
 
@@ -29,6 +34,25 @@ class CreateOrEditTaskFragment : BaseFragment() {
             false
         )
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        enterTransition = MaterialContainerTransform().apply {
+            // Manually add the Views to be shared since this is not a standard Fragment to
+            // Fragment shared element transition.
+            startView = requireActivity().findViewById(R.id.fab_create)
+            endView = binding.container
+            duration = resources.getInteger(R.integer.motion_duration_large).toLong()
+            scrimColor = Color.TRANSPARENT
+            containerColor = requireContext().themeColor(R.attr.colorSurface)
+            startContainerColor = requireContext().themeColor(R.attr.colorSecondary)
+            endContainerColor = requireContext().themeColor(R.attr.colorSurface)
+        }
+        returnTransition = Slide(Gravity.END).apply {
+            duration = resources.getInteger(R.integer.motion_duration_large).toLong()
+            addTarget(R.id.container)
+        }
     }
 
     override fun observeData() {
