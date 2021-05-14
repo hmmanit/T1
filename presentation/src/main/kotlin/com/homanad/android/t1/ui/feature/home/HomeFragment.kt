1304e83bfd7a7ea0ea19e57feb71d47873d2ec10
Man Ho <manhm@rch-asia.com>
2021-05-14 11:32:37 +0700

    Navigate to COE Task

--- presentation/src/main/kotlin/com/homanad/android/t1/ui/feature/home/HomeFragment.kt
+++ presentation/src/main/kotlin/com/homanad/android/t1/ui/feature/home/HomeFragment.kt
@@ -5,11 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import com.homanad.android.common.components.ui.BaseFragment
+import com.homanad.android.common.extensions.view.gone
+import com.homanad.android.common.extensions.view.visible
 import com.homanad.android.t1.R
 import com.homanad.android.t1.databinding.FragmentHomeBinding
 import com.homanad.android.t1.ui.feature.home.page.adapter.HomeViewPagerAdapter
+import com.homanad.android.t1.ui.feature.home.page.type.Page
 
 class HomeFragment : BaseFragment() {
 
@@ -50,6 +55,29 @@ class HomeFragment : BaseFragment() {
             tab.text = adapter.pages[position].title
 //            tab.icon = R.drawable.ic_home.asDrawable(requireContext())
         }.attach()
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                when (adapter.pages[position].id) {
+                    Page.TASKS.id -> {
+                        binding.fabCreate.visible()
+                        binding.fabCreate.setOnClickListener {
+                            val directions =
+                                HomeFragmentDirections.actionHomeFragmentToCreateOrEditTaskFragment()
+                            findNavController().navigate(directions)
+                        }
+                    }
+                    Page.BOARDS.id -> {
+                        binding.fabCreate.visible()
+                        binding.fabCreate.setOnClickListener {
+
+                        }
+                    }
+                    Page.OTHERS.id -> {
+                        binding.fabCreate.gone()
+                    }
+                }
+            }
+        })
     }
 
     companion object {
